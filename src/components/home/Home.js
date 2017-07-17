c2aa16800fc4a32c8a4432a85cf54cfe4a588837
Stephanie Randolph <stephanierandolph@Stephanies-MacBook-Pro.local>
2017-07-17 15:04:46 -0600

    add button list

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,29 +1,47 @@
 import React, { Component } from 'react';
-import { Alert, AppRegistry, Button, StyleSheet, View, TextInput, Text } from 'react-native';
+import { Alert, AppRegistry, Platform, StyleSheet, Text,TextInput, FlatList, TouchableHighlight, TouchableOpacity, TouchableNativeFeedback, TouchableWithoutFeedback, View, ScrollView, Image, } from 'react-native';
 
 
 class Home extends Component {
+    constructor(props){
+        super(props);
+        this.state = {text: ''};
+    }
     _onPressButton() {
-        Alert.alert('You tapped the button!')
+
+        Alert.alert("You tapped a button")
+    };
+
+    _onLongPressButton() {
+        Alert.alert('You long-pressed a button')
     }
     render() {
         return (
-            <View>
+            <ScrollView>
                 <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
-                <View style={{width: 75, height: 75, backgroundColor: 'skyblue'}} />
-                <View style={{width: 100, height: 100, backgroundColor: 'steelblue'}}/>
-                <View style = {styles.buttonContainer}>
-                    <Button
-                            onPress={this._onPressButton}
-                            title="Press Me"
-                    />
-                    <Button
-                        onPress={this._onPressButton}
-                        title="Press Me"
-                        color="#841584"
+                <View style={{width: 75, height: 75, backgroundColor: 'steelblue'}} />
+                <View style={{padding: 10}}>
+                    <TextInput
+                        style={{height: 80}}
+                        placeholder="Who are you creating a Tracker for?"
+                        onChangeText={(text) => this.setState({text})}
                     />
                 </View>
-            </View>
+                <FlatList
+                    data={[
+                            {key: 'Med Tracker'},
+                            {key: 'Immunization Tracker'},
+                            {key: 'Note Tracker'},
+                            {key: 'Allergy Tracker'},
+                            {key: 'Symptom Tracker'},
+                        ]}
+                     renderItem={({item}) =>   <TouchableOpacity onPress={this._onPressButton}>
+                         <View style={styles.button}>
+                             <Text style={styles.buttonText}>Create {item.key}</Text>
+                         </View>
+                     </TouchableOpacity>}
+                />
+            </ScrollView>
         );
     }
 }
@@ -33,14 +51,16 @@ export default Home
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
+        justifyContent: 'center'
     },
-    buttonContainer: {
-        margin: 30,
+    button: {
+        marginBottom: 30,
+        width: 250,
+        alignItems: 'center',
+        backgroundColor: '#2196F3'
     },
-    alternativeLayoutButtonContainer: {
-        margin: 20,
-        width: 200,
-        height: 200,
+    buttonText: {
+        padding: 20,
+        color: 'white'
     }
 })
